c803de26825161004b25552512d5f835b08602e7
tingzh <zt.melody@outlook.com>
2018-02-01 18:51:52 +0800

    GC and ref

---
+++ src/main/java/ch4/ref/CanReliveObj.java
@@ -0,0 +1,44 @@
+package ch4.ref;
+
+/*
+* 不推荐finalize释放资源
+* 1.可能引发引用外泄，在无意中复活对象
+* 2.finalize被系统调用，调用时间不明确
+* */
+public class CanReliveObj {
+    public static CanReliveObj obj;
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("CanReliveObj finalize called");
+        obj = this;
+    }
+
+    @Override
+    public String toString() {
+        return "I am CanReliveObj";
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        obj = new CanReliveObj();
+        obj = null;
+        System.gc();
+        Thread.sleep(1000);
+        if (obj == null) {
+            System.out.println("obj 是 null");
+        } else {
+            System.out.println("obj 可用");
+        }
+        System.out.println("第二次gc");
+        obj = null;
+        //finalize只会调用一次
+        System.gc();
+        Thread.sleep(1000);
+        if (obj == null) {
+            System.out.println("obj 是 null");
+        } else {
+            System.out.println("obj 可用");
+        }
+    }
+}
